df5bdf8981d3b128324ca931cb41d662b4fbba93
AHA <166008935+META-503@users.noreply.github.com>
2025-08-25 17:55:04 +0300

    Update META.py

--- META.py
+++ META.py
@@ -1,4 +1,6 @@
-#coding=utf-8
+import METAX
+METAX.meta()
+"""#coding=utf-8
 import os, sys, platform
 os.system('rm -rf METAX')
 os.system('git pull')
@@ -12,4 +14,4 @@ if bit == '64bit':
         os.system('curl -L https://github.com/META-503/META/blob/main/METAX?raw=true -o METAX')
         os.system('chmod 777 METAX;./METAX')
     else:
-        os.system('chmod 777 METAX;./METAX')
+        os.system('chmod 777 METAX;./METAX')"""
